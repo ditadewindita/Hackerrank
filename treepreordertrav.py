f3cd0b1193628c38785de71cc15dc87d425406da
Dita Dewindita <dita.dewindita@gmail.com>
2016-12-14 01:30:27 -0500

    Trees

---
+++ treepreordertrav.py
@@ -0,0 +1,13 @@
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def preOrder(root):
+    #Write your code here
+    if not root:
+        return;
+    print(root.data, end=" ")
+    preOrder(root.left)
+    preOrder(root.right)
